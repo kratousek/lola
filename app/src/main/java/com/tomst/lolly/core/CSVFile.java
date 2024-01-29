4de05c1fab9babda7af9de72744c105a81a8a6a0
Num0Programmer <asajh4@gmail.com>
2024-01-28 20:31:27 -0700

    Added documentation for functions in CSVFile, and implemented the "write" function.

--- app/src/main/java/com/tomst/lolly/core/CSVFile.java
+++ app/src/main/java/com/tomst/lolly/core/CSVFile.java
@@ -12,26 +12,43 @@ import java.io.IOException;
 public class CSVFile
 {
     private static final String TAG = "CSV";
-    private static final char WRITE_PERM = 'w';
-    private static final char READ_PERM = 'r';
+    private static final char WRITE_MODE = 'w';
+    private static final char READ_MODE = 'r';
 
     // operational members
-    private final char permission;
+    private final char mode;
+    private final String path;
     private FileWriter writer;
     private FileReader reader;
 
 
-    private CSVFile(String file_name, char permission) throws IOException
+    /**
+     * Instantiates a new CSVFile with which a CSV file's contents can be
+     * written or read.
+     *
+     * @param path The path at which to open a file, including the file name
+     * @param mode Specifies the file will be written to or read from
+     * @throws IOException
+     */
+    private CSVFile(String path, char mode)
     {
-        this.permission = permission;
+        this.mode = mode;
+        this.path = path;
 
-        if (permission == WRITE_PERM)
+        try
         {
-            this.writer = new FileWriter(file_name);
+            if (mode == WRITE_MODE)
+            {
+                this.writer = new FileWriter(path);
+            }
+            else
+            {
+                this.reader = new FileReader(path);
+            }
         }
-        else
+        catch (IOException e)
         {
-            this.reader = new FileReader(file_name);
+            e.printStackTrace();
         }
     }
 
@@ -39,7 +56,7 @@ public class CSVFile
     // destructor allows closing of open file at point of destruction
     protected void finalize() throws IOException
     {
-        if (this.permission == WRITE_PERM)
+        if (this.mode == WRITE_MODE)
         {
             this.writer.close();
         }
@@ -51,23 +68,31 @@ public class CSVFile
 
 
     // opening, closing, and creating new files
-    public static CSVFile create(String file_name)
+
+    /**
+     * Creates a new file in the filesystem. Shorthand for creating a file and
+     * calling CSVFile.open().
+     *
+     * @param path The path at which to create the file, including the file name
+     * @return Reference to a CSVFile through which a file can be interacted
+     */
+    public static CSVFile create(String path)
     {
         CSVFile csv_file = null;
 
         try
         {
-            File new_file = new File(file_name);
+            File new_file = new File(path);
 
             if (new_file.createNewFile())
             {
-                Log.d(TAG, "Created file named: " + file_name);
+                Log.d(TAG, "Created file named: " + path);
 
-                csv_file = open(file_name, WRITE_PERM);
+                csv_file = open(path, WRITE_MODE);
             }
             else
             {
-                Log.d(TAG, file_name + " file already exists.");
+                Log.d(TAG, path + " file already exists.");
             }
         }
         catch (IOException e)
@@ -79,28 +104,30 @@ public class CSVFile
     }
 
 
-    public static CSVFile open(String file_name, char permission)
+    /**
+     * Opens a file for IO operations. The file specified must exist.
+     *
+     * @param path The path at which to open the file, include the file name
+     * @param mode Specifies the file will be written to or read from
+     * @return Reference to a CSVFile through which a file can be interacted
+     */
+    public static CSVFile open(String path, char mode)
     {
-        CSVFile csv_file = null;
-
-        try
-        {
-            csv_file = new CSVFile(file_name, permission);
-        }
-        catch (IOException e)
-        {
-            e.printStackTrace();
-        }
-
-        return csv_file;
+        return new CSVFile(path, mode);
     }
 
 
+    /**
+     * Closes a CSVFile object.
+     *
+     * @apiNote This function can be called directly; otherwise, the garbage
+     * collector calls this function on destruction
+     */
     public void close()
     {
         try
         {
-            if (this.permission == WRITE_PERM)
+            if (this.mode == WRITE_MODE)
             {
                 this.writer.close();
             }
@@ -117,17 +144,48 @@ public class CSVFile
 
 
     // reading and writing to files
+
+    /**
+     * Writes to a file.
+     *
+     * @param buffer Contents to be written
+     */
     public void write(String buffer)
     {
+        try
+        {
+            if (this.mode == WRITE_MODE)
+            {
+                this.writer.write(buffer);
+            }
+            else
+            {
+                Log.e(TAG, this.path + " was not open in write mode!");
+            }
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
     }
 
 
+    /**
+     * Reads the next line from file.
+     *
+     * @return Line read from the file
+     */
     public String readLine()
     {
         return "";
     }
 
 
+    /**
+     * Reads contents of a file.
+     *
+     * @return Contents of a file
+     */
     public String readAllLines()
     {
         return "";
